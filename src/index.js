6667d528df14b44ab1109722e2f9e6e62bd61757
Taras Kozmovych <taraskom0404@gmail.com>
2022-09-03 02:02:53 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,53 +1,110 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-const axios = require('axios');
-const BASE_URL = 'https://pixabay.com/api/?';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import fetchImages from './js/fetchImages';
+import Notiflix from 'notiflix';
 
-async function searchData(data, page) {
-    const params = new URLSearchParams({
-        key: '29665517-503a19893477763dfc63054fb',
-        q: data,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        page: page,
-        per_page: 40,
-    });
+const form = document.getElementById('search-form');
+const input = document.querySelector('input[type=text]');
+const loadMoreBtn = document.querySelector('button.load__more');
+const gallery = document.querySelector('div.gallery');
 
-    return await axios.get(`${BASE_URL}${params}`);
-};
-function createMarkup(images) {
-    return images
-        .map(image => {
-            return `
-        <div class="gallery__item">
-          <a class="gallery__link" href="${image.largeImageURL}">
-              <img class="gallery__image" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-          </a>
-          <div class="gallery__info">
-              <p class="gallery__info-item">
-                  <b>Likes ${image.likes}</b>
-              </p>
-              <p class="gallery__info-item">
-                  <b>Views ${image.views}</b>
-              </p>
-              <p class="gallery__info-item">
-                  <b>Comments ${image.comments}</b>
-              </p>
-              <p class="gallery__info-item">
-                  <b>Downloads ${image.downloads}</b>
-              </p>
-          </div>
-      </div>`;
+const cleanMarkup = ref => (ref.innerHTML = '');
+let pageNumber;
+let searchQuery;
+
+form.addEventListener('submit', inputHandler);
+
+function inputHandler(event) {
+    event.preventDefault();
+    cleanMarkup(gallery);
+
+    pageNumber = '1';
+    searchQuery = input.value;
+
+    if (!searchQuery) {
+        loadMoreBtn.style.visibility = 'hidden';
+        return;
+    }
+
+    fetchImages(searchQuery, pageNumber)
+        .then(data => {
+            console.log(data);
+            createImgMarkup(data);
+            if (data.data.totalHits > 0) {
+                Notiflix.Notify.info(`Hooray! We found ${data.data.totalHits} images.`);
+            }
+            loadMoreBtn.style.visibility = 'visible';
         })
-        .join('');
-};
-const refs = {
-    form: document.querySelector('#search-form'),
-    gallery: document.querySelector('.gallery'),
-    loadMore: document.querySelector('.load-more'),
-};
-const gallery = new SimpleLightbox('.gallery a', {
-    scrollZoom: false,
-    captionsData: 'alt',
-    captionDelay: 250,
+        .catch(err => {
+            if (err.code === 'ERR_BAD_REQUEST') {
+                Notiflix.Notify.success(
+                    "We're sorry, but you've reached the end of search results."
+                );
+                loadMoreBtn.style.visibility = 'hidden';
+            }
+        });
+
+    return searchQuery;
+}
+
+loadMoreBtn.addEventListener('click', () => {
+    pageNumber++;
+    fetchImages(searchQuery, pageNumber)
+        .then(data => {
+            console.log(data);
+            createImgMarkup(data);
+        })
+        .catch(err => {
+            if (err.code === 'ERR_BAD_REQUEST') {
+                Notiflix.Notify.success(
+                    "We're sorry, but you've reached the end of search results."
+                );
+                loadMoreBtn.style.visibility = 'hidden';
+            }
+        });
 });
+
+function createImgMarkup(data) {
+    data.data.hits.forEach(hit => {
+        const picWrapper = document.createElement('div');
+        picWrapper.classList.add('gallery__item');
+
+        const pic = document.createElement('a');
+        pic.setAttribute('href', `${hit.largeImageURL}`);
+        pic.innerHTML = `
+      <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
+    `;
+
+        const info = document.createElement('div');
+        info.classList.add('info');
+        info.innerHTML = `
+        <p class="info-item">
+          <b>Likes</b><span>${hit.likes}</span>
+        </p>
+        <p class="info-item">
+          <b>Views</b><span>${hit.views}</span>
+        </p>
+        <p class="info-item">
+          <b>Comments</b><span>${hit.comments}</span>
+        </p>
+        <p class="info-item">
+          <b>Downloads</b><span>${hit.downloads}</span>
+        </p>
+      `;
+        gallery.appendChild(picWrapper);
+        picWrapper.appendChild(pic);
+        picWrapper.appendChild(info);
+    });
+    new SimpleLightbox('.gallery div a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+    const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+        top: cardHeight * 3,
+        behavior: 'smooth',
+    });
+}
